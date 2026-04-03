0273cc1cede6dc40e13216de1920306e57772f82
luan-cavalcante-dev <luansantos1802@gmail.com>
2026-04-03 19:10:22 -0300

    atualização sql universidade, adicionando tabelas diciplinas e matricula_disciplina

--- universidade.sql
+++ universidade.sql
@@ -68,6 +68,30 @@ INSERT INTO `cursos` VALUES (1,'Pedagogia',200),(2,'Historia',200),(3,'Sitema Pa
 UNLOCK TABLES;
 
 --
+-- Table structure for table `disciplinas`
+--
+
+DROP TABLE IF EXISTS `disciplinas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `disciplinas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `disciplinas`
+--
+
+LOCK TABLES `disciplinas` WRITE;
+/*!40000 ALTER TABLE `disciplinas` DISABLE KEYS */;
+INSERT INTO `disciplinas` VALUES (1,'Banco de dados'),(2,'Logica de Programacao'),(3,'Didatica'),(4,'Historia Antiga'),(5,'Fundamentos de Computacao');
+/*!40000 ALTER TABLE `disciplinas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `matricula`
 --
 
@@ -97,6 +121,35 @@ LOCK TABLES `matricula` WRITE;
 INSERT INTO `matricula` VALUES (1,1,1,'2024-01-10','2024.1'),(2,2,1,'2024-01-11','2024.1'),(3,3,2,'2024-01-12','2024.1'),(4,4,2,'2024-01-13','2024.1'),(5,5,3,'2024-01-14','2024.1'),(6,6,3,'2024-01-15','2024.1'),(7,7,1,'2024-01-16','2024.1'),(8,8,2,'2024-01-17','2024.1'),(9,9,3,'2024-01-18','2024.1'),(10,10,1,'2024-01-19','2024.1'),(11,1,2,'2024-02-01','2024.1'),(12,2,3,'2024-02-02','2024.1'),(13,3,1,'2024-02-03','2024.1'),(14,4,3,'2024-02-04','2024.1'),(15,5,1,'2024-02-05','2024.1'),(16,6,2,'2024-02-06','2024.1'),(17,7,3,'2024-02-07','2024.1'),(18,8,1,'2024-02-08','2024.1'),(19,9,2,'2024-02-09','2024.1'),(20,10,3,'2024-02-10','2024.1'),(21,1,4,'2026-01-20','2026.1'),(22,3,4,'2026-01-21','2026.1'),(23,9,4,'2026-01-20','2026.1'),(24,10,4,'2026-01-28','2026.1');
 /*!40000 ALTER TABLE `matricula` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `matricula_disciplina`
+--
+
+DROP TABLE IF EXISTS `matricula_disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `matricula_disciplina` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `matricula_id` int DEFAULT NULL,
+  `disciplina_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `matricula_id` (`matricula_id`),
+  KEY `disciplina_id` (`disciplina_id`),
+  CONSTRAINT `matricula_disciplina_ibfk_1` FOREIGN KEY (`matricula_id`) REFERENCES `matricula` (`id`),
+  CONSTRAINT `matricula_disciplina_ibfk_2` FOREIGN KEY (`disciplina_id`) REFERENCES `disciplinas` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `matricula_disciplina`
+--
+
+LOCK TABLES `matricula_disciplina` WRITE;
+/*!40000 ALTER TABLE `matricula_disciplina` DISABLE KEYS */;
+INSERT INTO `matricula_disciplina` VALUES (1,22,1),(2,23,1),(3,20,2),(4,3,1),(5,3,2),(6,4,4),(7,8,2);
+/*!40000 ALTER TABLE `matricula_disciplina` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -107,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-03-30 23:49:36
+-- Dump completed on 2026-04-03 22:06:48
